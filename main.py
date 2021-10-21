5019d10ab85039675c67a75d668ae36d3d9fb07f
Sorkhemiri <sorkhemiri@gmail.com>
2021-10-21 08:10:43 +0330

    feat: error handeling complete

--- main.py
+++ main.py
@@ -4,7 +4,12 @@ from utils import get_input, create_graph
 
 if __name__ == "__main__":
     edges = get_input()
+    if not edges:
+        exit(1)
     graph = create_graph(edges=edges)
+    if graph.has_broken():
+        print("graph is broken already!")
+        exit(1)
     shopstan_edges = []
     for edge in graph.edges:
         g = deepcopy(graph)
