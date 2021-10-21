1ade1a8b2d6eaed527faa47bfa320ca39108e318
Sorkhemiri <sorkhemiri@gmail.com>
2021-10-21 07:21:25 +0330

    feat: dijkstra completed

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+from models import Vertex
 from utils import get_input, create_graph
 
 if __name__ == "__main__":
     # edges = get_input()
-    edges = [(1, 2), (1, 2), (3, 4), (5, 4)]
+    edges = [(1, 2), (1, 2), (3, 2), (5, 6), (4, 2)]
     graph = create_graph(edges=edges)
+    graph.has_broken()
--- models/graph.py
+++ models/graph.py
@@ -1,3 +1,4 @@
+from math import isinf
 from typing import List
 
 from .edge import Edge
@@ -8,6 +9,7 @@ class Graph:
     def __init__(self, vertices: List[Vertex] = None, edges: List[Edge] = None):
         self.vertices = vertices if vertices else []
         self.edges = edges if edges else []
+        self._shortest_path = [float("inf")] * len(self.vertices)
 
     def add_edge(self, edge: Edge):
         self.edges.append(edge)
@@ -27,3 +29,42 @@ class Graph:
                     return True
         else:
             return False
+
+    def min_distance(self, dist: List[float], spt_set: List[bool]):
+        minimum = float("inf")
+        min_indices = []
+        for i, v in enumerate(self.vertices):
+            if dist[i] < minimum and spt_set[i] is False:
+                minimum = dist[i]
+                min_indices = [i]
+            elif dist[i] == minimum and spt_set[i] is False:
+                min_indices.append(i)
+        return min_indices
+
+    def dijkstra(self, v: Vertex):
+        dist = [float("inf")] * len(self.vertices)
+        vertex_index = self.vertices.index(v)
+        dist[vertex_index] = 0
+        spt_set = [False] * len(self.vertices)
+
+        for i_vertex in self.vertices:
+            inds = self.min_distance(dist, spt_set)
+            for ind in inds:
+                spt_set[ind] = True
+            for ind in inds:
+                for j, j_vertex in enumerate(self.vertices):
+                    e1 = Edge(self.vertices[ind], j_vertex)
+                    e2 = Edge(j_vertex, self.vertices[ind])
+                    if (e1 in self or e2 in self) and spt_set[j] is False:
+                        if e1 in self and dist[j] > dist[ind] + e1.distance:
+                            dist[j] = dist[ind] + e1.distance
+                        if e2 in self and dist[j] > dist[ind] + e2.distance:
+                            dist[j] = dist[ind] + e2.distance
+        self._shortest_path = dist
+
+    def has_broken(self):
+        self.dijkstra(self.vertices[0])
+        for item in self._shortest_path:
+            if isinf(item):
+                return True
+        return False
