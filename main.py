85dcef89ce4b3b15a366de292967a44ea35ecd41
Sorkhemiri <sorkhemiri@gmail.com>
2021-10-21 07:36:13 +0330

    feat: shopstan complete

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
+from copy import deepcopy
+
 from models import Vertex, Edge
 from utils import get_input, create_graph
 
 if __name__ == "__main__":
-    # edges = get_input()
-    edges = [(1, 2), (1, 2), (3, 2), (5, 6), (4, 2)]
+    edges = get_input()
     graph = create_graph(edges=edges)
-    v1 = Vertex(name=1)
-    v2 = Vertex(name=2)
-    e = Edge(v1, v2)
-    graph.remove_edge(e)
-    graph.edges
+    shopstan_edges = []
+    for edge in graph.edges:
+        g = deepcopy(graph)
+        g.remove_edge(edge=edge)
+        if g.has_broken():
+            shopstan_edges.append(edge)
+
+    for edge in shopstan_edges:
+        print(edge.vertices[0].name, edge.vertices[1].name)
